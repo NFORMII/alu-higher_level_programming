bad3838339290b72429290c4b0c55442f8532aa4
NFORMII <n.girbong@alustudent.com>
2024-06-19 15:29:01 -0700

    edges

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
-last 2 letters = word[:-2]
-midle word = word[1:-1]
+word_last_2 = word[-2]
+midle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
