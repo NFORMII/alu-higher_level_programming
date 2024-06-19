58c4056bbda79eb84bc711999768bfb5fd9d673d
NFORMII <n.girbong@alustudent.com>
2024-06-19 15:15:22 -0700

    edges

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+last 2 letters = word[-2]
+midle word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
