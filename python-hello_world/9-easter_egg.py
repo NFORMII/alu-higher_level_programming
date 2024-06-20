d401c9683d979e54a7b6f344b5bc769aa2bc333a
NFORMII <n.girbong@alustudent.com>
2024-06-20 13:36:46 -0700

    easter

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
-print("The Zen of Python, by Tim Peters\nBeautiful is better than ugly.\nExplicit is better than implicit.")
-
+a = ("The Zen of Python, by Tim Peters\nBeautiful is better than ugly.\nExplicit is better than implicit.")
+print(a)
