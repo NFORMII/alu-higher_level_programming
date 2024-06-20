225c8bdaac9ffafdb1d2799609c62954908648aa
NFORMII <n.girbong@alustudent.com>
2024-06-20 13:25:23 -0700

    easter

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 a = "The Zen of Python, by Tim Peters\nBeautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested."
+Explicit is better than implicit."
 print(a)
