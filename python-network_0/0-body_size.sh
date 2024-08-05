95082467b48f3024cacd539775092c7994ebb15c
Nformi Modestine Girbong <n.girbong@alustudent.com>
2024-08-05 13:21:33 +0200

    network

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This script takes a URL, sends a request, and displays the content of the response.
-curl -s "$1"
+#Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
 
