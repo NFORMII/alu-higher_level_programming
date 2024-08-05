db953e9e5a4ae667ce02d4eecf3cb9a0f65b5e04
Nformi Modestine Girbong <n.girbong@alustudent.com>
2024-08-05 13:19:04 +0200

    networl

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# This script takes a URL, sends a request, and displays the size of the body of the response in bytes.
-curl -s -o /dev/null -w "%{size_download} bytes\n" "$1"
+# This script takes a URL, sends a request, and displays the content of the response.
+curl -s "$1"
+
