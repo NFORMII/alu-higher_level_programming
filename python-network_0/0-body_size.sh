2eeaf84caff32a160db901a433263b559e8322d0
Nformi Modestine Girbong <n.girbong@alustudent.com>
2024-08-05 13:05:22 +0200

    o network

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s "$1" | wc -c
+# This script takes a URL, sends a request, and displays the size of the body of the response in bytes.
+curl -s -o /dev/null -w "%{size_download} bytes\n" "$1"
